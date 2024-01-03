c7b8440f5f3be202e7ac58dfe832543a44932797
marcusjacobsen <S215799@dtu.dk>
2024-01-03 14:04:03 +0100

    fix det hele

--- features/legacy.feature
+++ features/legacy.feature
@@ -1,9 +1,82 @@
 Feature: Gilded Rose behaviour of the legacy code
 
-// Example of a scenario
 Scenario: Aged Brie increases in quality the older it gets
 	Given an item named "Aged Brie" with quality 9 and which has to be sold in 11 days
 	When a day has passed
 	Then it has to be sold in 10 days
 	And its quality is 10
-	
+
+# A brie past its sell by date increases in quality twice as fast
+Scenario: Aged Brie increases in quality twice as fast after sell by date
+	Given an item named "Aged Brie" with quality 9 and which has to be sold in 0 days
+	When a day has passed
+	Then it has to be sold in -1 days
+	And its quality is 11
+
+Scenario: Aged Brie never goes above 50 in quality
+	Given an item named "Aged Brie" with quality 50 and which has to be sold in 5 days
+	When a day has passed
+	Then it has to be sold in 4 days
+	And its quality is 50
+
+Scenario: Backstage passes increases in quality the older it gets
+	Given an item named "Backstage passes to a TAFKAL80ETC concert" with quality 30 and which has to be sold in 40 days
+	When a day has passed
+	Then it has to be sold in 39 days
+	And its quality is 31
+
+Scenario: Backstage passes increases in quality by two when its sellIn is 10 days or less
+	Given an item named "Backstage passes to a TAFKAL80ETC concert" with quality 30 and which has to be sold in 10 days
+	When a day has passed
+	Then it has to be sold in 9 days
+	And its quality is 32
+
+Scenario: Backstage passes increases in quality by three when its sellIn is 5 days or less
+	Given an item named "Backstage passes to a TAFKAL80ETC concert" with quality 30 and which has to be sold in 5 days
+	When a day has passed
+	Then it has to be sold in 4 days
+	And its quality is 33
+
+Scenario: Backstage passes drops to 0 in quality after the concert
+	Given an item named "Backstage passes to a TAFKAL80ETC concert" with quality 30 and which has to be sold in 0 days
+	When a day has passed
+	Then it has to be sold in -1 days
+	And its quality is 0
+
+	# 	- Once the sell by date has passed, Quality degrades twice as fast
+Scenario: Normal items degrade twice as fast when sell by date has passed
+	Given an item named "Normal Item" with quality 10 and which has to be sold in 0 days
+	When a day has passed
+	Then it has to be sold in -1 days
+	And its quality is 8
+
+
+Scenario: Stinky Rat decreases in quality the older it gets
+	Given an item named "Stinky Rat" with quality 5 and which has to be sold in 3 days
+	When a day has passed
+	Then it has to be sold in 2 days
+	And its quality is 4
+
+Scenario: Stinky Rat never goes below 0 in quality
+	Given an item named "Stinky Rat" with quality 0 and which has to be sold in 3 days
+	When a day has passed
+	Then it has to be sold in 2 days
+	And its quality is 0
+
+Scenario: Sulfuras doesn't change in quality or sellIn
+	Given an item named "Sulfuras, Hand of Ragnaros" with quality 100 and which has to be sold in 500 days
+	When a day has passed
+	Then it has to be sold in 500 days
+	And its quality is 100
+
+
+#Scenario: Conjured items degrade in quality twice as fast as normal items
+#	Given an item named "Conjured Spirit Rat" with quality 10 and which has to be sold in 3 days
+#	When a day has passed
+#	Then it has to be sold in 2 days
+#	And its quality is 8
+
+
+
+
+
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,56 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+
+            int quality_change = 0;
+            int sellIn_change = 0;
+
+            // determine changes
+            switch(items[i].name) {
+                case "Aged Brie":
+                    quality_change = 1;
+                    sellIn_change = -1;
+                    if(items[i].sellIn <= 0) {
+                        quality_change = 2;
                     }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (items[i].sellIn <= 0) {
+                        quality_change = -items[i].quality;
+                    } else if (items[i].sellIn <= 5) {
+                        quality_change = 3;
+                    } else if (items[i].sellIn <= 10) {
+                        quality_change = 2;
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        quality_change = 1;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    sellIn_change = -1;
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    return;
+                default:
+                    if (items[i].sellIn <= 0) {
+                        quality_change = -2;
+                    } else {
+                        quality_change = -1;
                     }
-                }
+                    sellIn_change = -1;
+                    break;
             }
+
+            // apply changes
+            items[i].quality += quality_change;
+            items[i].sellIn += sellIn_change;
+
+
+            // check for quality bounds
+            if(items[i].quality > 50) {
+                items[i].quality = 50;
+            } else if (items[i].quality < 0) {
+                items[i].quality = 0;
+            }
+
+
         }
     }
 }
